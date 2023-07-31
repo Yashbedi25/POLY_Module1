0d4fed9b6ba18881f8bef3aa4cee7404f108712b
Yash_Bedi <105603754+Yashbedi25@users.noreply.github.com>
2023-07-31 19:56:36 +0530

    Create nfts.js

---
+++ nfts.js
@@ -0,0 +1,29 @@
+const nfts = [
+    {
+        name: '0',
+        description: 'a horse dancing tektonic',
+        image: 'QmTro1TRDaHYDvbXKiundgJWUR6osquozXBLJkKqhinXNV'
+    },
+    {
+        name: '1',
+        description: 'a horse dancing tektonic',
+        image: 'QmWgW6hUqSLp8KhS47qxuibJ9ssJxTtouUDB5oTRVQNzuv'
+    },//change
+    {
+        name: '2',
+        description: 'a horse dancing tektonic',
+        image: 'QmSYfv6YdXuT9PyybEv7kxMuqC5794JeyDHhYC9wikBRrJ'
+    },
+    {
+        name: '3',
+        description: 'a horse dancing tektonic',
+        image: 'QmazSfKbMAXHPzkZxZMi3UsMjEqxikGt82sJBFdYEixmzo'
+    },
+    {
+        name: '4',
+        description: 'a horse dancing tektonic',
+        image: 'Qmbg2FuFoP5nw6fyeNVmdJDE8S6ZJ1dQ2EW3xYw8vdvzLG'
+    },    
+];
+
+module.exports = nfts;
